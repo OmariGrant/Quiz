09ce357c99f72a425f051a75b41effd658f1c35b
OmariGrant <OmariGrant@users.noreply.github.com>
2017-11-12 13:49:53 +0000

    removed comments

--- main.py
+++ main.py
@@ -19,31 +19,15 @@ def register():
     login_file = open("login.csv", "a", newline='')
     
     #put into dictionary
-    reg_details = {'Name': name, 'Age': age, 'Year': year, 'Username': username, 'Password': password}
-    login_details = {'Username' : username, 'Password' : password}
-
-
-    #verify registration - if 3 characters of name in use, use more
-   # verify_reg = pickle.load(open ("reg.txt", "rb"))
- #   if(username in verify_reg):
-  #      for x in range(3, 10):
-   #         if(name[:x] not in verify_reg):
-    #            username = name[:x] + age
-     #           reg_details = {username: password}
-                #store into a file
-      #          print("Your Username is: " + username)
-       #         pickle.dump(reg_details, open ("reg.txt", "ab+"))
-        #        break
+    login_details = {'Username' : username, 'Password' : password, 'Name': name, 'Age': age, 'Year': year}
 
     #store into a file
     print("Your Username is: " + username)
-    #pickle.dump(login_details, open ("logins.txt", "ab+"))
     with login_file:
-        login_file_header = ['Username', 'Password']
+        login_file_header = ['Username', 'Password', 'Name', 'Age', 'Year']
         login_file_writer = csv.DictWriter(login_file, fieldnames=login_file_header)
         if not login_file_exists:
             login_file_writer.writeheader()
         login_file_writer.writerow(login_details)    
-    #pickle.dump(reg_details, open (reg_file, "ab+"))
                 
 register()
